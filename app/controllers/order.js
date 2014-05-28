eb12c65acc9e338c227bc01d83979e83a475158e
Tom Close <tom.close@cantab.net>
2014-05-28 15:35:52 +0100

    Add bootstrap; allow items to be added

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -1,5 +1,22 @@
 var OrderController = Ember.ObjectController.extend({
+  needs: ['dishes'],
 
+  dishes: function() {
+    return this.get('controllers.dishes');
+  }.property('controllers.dishes'),
+
+
+  actions: {
+    addDish: function() {
+      var newDish = this.get('newDish');
+      this.get('items').pushObject(
+        this.store.createRecord('item', {
+          dish: newDish,
+          quantity: 1,
+        })
+      );
+    }
+  }
 });
 
 export default OrderController;
--- app/models/item.js
+++ app/models/item.js
@@ -1,4 +1,9 @@
 export default DS.Model.extend({
   dish:     DS.belongsTo('dish'),
   quantity: DS.attr('number'),
+
+
+  priceCents: function() {
+    return this.get('quantity') * this.get('dish.priceCents');
+  }.property('quantity, dish.priceCents'),
 });
